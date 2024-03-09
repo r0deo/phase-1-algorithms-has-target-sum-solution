6e6d712f909c51551e8dffaa6e415a3c15b83a7a
Ali Izzaldin <ali.izzaldin7@gmail.com>
2024-03-09 11:26:31 +0300

    again

--- index.js
+++ index.js
@@ -1,34 +1,49 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+  for (let i = 0; i < array.length; i++) {
+   for (let j = i + 1; j < array.length; j++) {
+      if (array[i] + array[j] === target) {
+       return true;
+     }
+   }
+ }
+  return false;
 }
 
-/* 
-  Write the Big O time complexity of your function here
-*/
+console.log(hasTargetSum([3, 8, 12, 4, 11, 7], 10));  
+console.log(hasTargetSum([22, 19, 4, 6, 30], 25));    
+console.log(hasTargetSum([1, 2, 5], 4));              
+console.log(hasTargetSum([-7, 10, 4, 8], 3));         
+console.log(hasTargetSum([1, 2, 3, 4], 5));           
+console.log(hasTargetSum([2, 2, 3, 3], 4));           
+console.log(hasTargetSum([4], 4));                    
+
 
 /* 
-  Add your pseudocode here
+ Write the Big O time complexity of your function here
+ it's O(n^2) you have to iterate twice over the array to find the sum or the target 
 */
+
+
 
 /*
-  Add written explanation of your solution here
+ Add written explanation of your solution here
 */
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
-  // add your own custom tests in here
-  console.log("Expecting: true");
-  console.log("=>", hasTargetSum([3, 8, 12, 4, 11, 7], 10));
+ // add your own custom tests in here
+ console.log("Expecting: true");
+ console.log("=>", hasTargetSum([3, 8, 12, 4, 11, 7], 10));
 
-  console.log("");
+ console.log("");
 
-  console.log("Expecting: true");
-  console.log("=>", hasTargetSum([22, 19, 4, 6, 30], 25));
+ console.log("Expecting: true");
+ console.log("=>", hasTargetSum([22, 19, 4, 6, 30], 25));
 
-  console.log("");
+ console.log("");
 
-  console.log("Expecting: false");
-  console.log("=>", hasTargetSum([1, 2, 5], 4));
+ console.log("Expecting: false");
+ console.log("=>", hasTargetSum([1, 2, 5], 4));
 }
 
 module.exports = hasTargetSum;
